64913dc051b5a4d8d991f88add493b85c9e903d3
Tyler <lllwvlvwlll@gmail.com>
2019-11-01 16:03:52 -0600

    refactor

--- test/change_group.js
+++ test/change_group.js
@@ -2,26 +2,22 @@ var assert = require("assert");
 var ast = require("../index.js")
 
 
-describe("Ping", () => {
-  let email = "tyler@moonlight.io";
+describe("change user group", () => {
+  let email = "worker@moonlight.io";
   let password = "password";
   let secret = "7A8D5C74979ABBF98F419BDD862BA234";
   let asteroid = new ast.Asteroid(ast.Configs.Dev);
   let user = null;
 
-  it("should register a new account", async () => {
+  it("should register a new worker", async () => {
     let registerRes = await asteroid.RegisterEmailWithSecret(email, secret);
     let updateRes = await asteroid.UpdatePassword(registerRes.dynamic_token, password, 'NewAccount')
   });
 
-  it("should log into the account", async () => {
+  it("should log into the account and elevate its privs", async () => {
     user = await asteroid.LoginEmail(email, password)
+    let privUpgradeRes = await user.SetUserGroupByEmail(email,"worker", secret)
+    console.log(privUpgradeRes)
   });
-
-  it("should create a new profile for the account", async () => {
-    let profile = await user.CreateProfile();
-    console.log(profile)
-  })
-
 });
 
