bc8a02ad104cd8cc833688ff20bef88994c5b40f
Tyler <lllwvlvwlll@gmail.com>
2019-10-31 21:03:53 -0600

    async

--- test/ping.js
+++ test/ping.js
@@ -1,10 +1,13 @@
 var assert = require("assert");
 var ast = require("../index.js")
 
-describe("Ping", () => {
-  let asteroid = new ast.Asteroid(ast.Configs.Dev)
 
-  user = asteroid.LoginEmail("tyler@moonlight.io", "password")
+describe("Ping", async () => {
 
+  let asteroid = new ast.Asteroid()
+  let res = asteroid.api.adu.LoginEmail(ast.Configs.Dev, "test@moonlight.io", "password")
+  console.log(res)
+  //let res = asteroid.api.adu.LoginEmail(ast.Configs.Dev, "tyler@moonlight.io", "password")
+  //console.log(res)
 
 })
