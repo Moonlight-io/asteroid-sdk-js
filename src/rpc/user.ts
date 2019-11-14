e6f9474c870a6fa9a1023dd98d484ec27f3454e1
Travis <travis@localhost.dev>
2019-11-14 13:56:08 +1100

    Better defaults and override capability for RPC methods

--- src/rpc/user.ts
+++ src/rpc/user.ts
@@ -1,3 +1,4 @@
+import { AxiosRequestConfig } from 'axios'
 import { invoke } from './base'
 import {
   RegisterEmailRequest,
@@ -21,65 +22,63 @@ import {
   LogoutRequest,
   LogoutResponse,
 } from '../interfaces'
-
-const defaultId: string = '0'
-const defaultMethodVersion: number = 1
+import { rpcDefaults } from '../constants'
 
 export class AsteroidUserRpc {
   // #region Register
 
-  static async registerEmail(rpcUrl: string, params: RegisterEmailRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<RegisterEmailResponse> {
+  static async registerEmail(rpcUrl: string, params: RegisterEmailRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<RegisterEmailResponse> {
     const method = 'User.RegisterEmail'
-    return await invoke(rpcUrl, method, params, id, methodVersion)
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
   }
 
-  static async registerEmailWithSecret(rpcUrl: string, params: RegisterEmailWithSecretRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<RegisterEmailWithSecretResponse> {
+  static async registerEmailWithSecret(rpcUrl: string, params: RegisterEmailWithSecretRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<RegisterEmailWithSecretResponse> {
     const method = 'User.RegisterEmailWithSecret'
-    return await invoke(rpcUrl, method, params, id, methodVersion)
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
   }
 
-  static async updatePassword(rpcUrl: string, params: UpdatePasswordRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<UpdatePasswordResponse> {
+  static async updatePassword(rpcUrl: string, params: UpdatePasswordRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<UpdatePasswordResponse> {
     const method = 'User.UpdatePassword'
-    return await invoke(rpcUrl, method, params, id, methodVersion)
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
   }
 
-  static async updatePasswordJwt(rpcUrl: string, params: UpdatePasswordJwtRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<UpdatePasswordJwtResponse> {
+  static async updatePasswordJwt(rpcUrl: string, params: UpdatePasswordJwtRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<UpdatePasswordJwtResponse> {
     const method = 'User.UpdatePasswordJWT'
-    return await invoke(rpcUrl, method, params, id, methodVersion)
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
   }
 
-  static async requestPasswordReset(rpcUrl: string, params: RequestPasswordResetRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<RequestPasswordResetResponse> {
+  static async requestPasswordReset(rpcUrl: string, params: RequestPasswordResetRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<RequestPasswordResetResponse> {
     const method = 'User.RequestPasswordReset'
-    return await invoke(rpcUrl, method, params, id, methodVersion)
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
   }
 
   // #endregion
 
   // #region Authenticate
 
-  static async loginEmail(rpcUrl: string, params: LoginEmailRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<LoginEmailResponse> {
+  static async loginEmail(rpcUrl: string, params: LoginEmailRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<LoginEmailResponse> {
     const method = 'User.LoginEmail'
-    return await invoke(rpcUrl, method, params, id, methodVersion)
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
   }
 
-  static async loginOauth(rpcUrl: string, params: LoginOauthRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<LoginOauthResponse> {
+  static async loginOauth(rpcUrl: string, params: LoginOauthRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<LoginOauthResponse> {
     const method = 'User.LoginOauth'
-    return await invoke(rpcUrl, method, params, id, methodVersion)
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
   }
 
-  static async setUserGroupByEmail(rpcUrl: string, params: SetUserGroupByEmailRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<SetUserGroupByEmailResponse> {
+  static async setUserGroupByEmail(rpcUrl: string, params: SetUserGroupByEmailRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<SetUserGroupByEmailResponse> {
     const method = 'User.SetUserGroupByEmail'
-    return await invoke(rpcUrl, method, params, id, methodVersion)
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
   }
 
-  static async newAccessToken(rpcUrl: string, params: NewAccessTokenRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<NewAccessTokenResponse> {
+  static async newAccessToken(rpcUrl: string, params: NewAccessTokenRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<NewAccessTokenResponse> {
     const method = 'User.NewAccessToken'
-    return await invoke(rpcUrl, method, params, id, methodVersion)
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
   }
 
-  static async logout(rpcUrl: string, params: LogoutRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<LogoutResponse> {
+  static async logout(rpcUrl: string, params: LogoutRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<LogoutResponse> {
     const method = 'User.Logout'
-    return await invoke(rpcUrl, method, params, id, methodVersion)
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
   }
 
   // #endregion
