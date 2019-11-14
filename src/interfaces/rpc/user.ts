8b69048ea3b6cfbc23ce5e7d8849f1f6580bbf84
Travis <travis@localhost.dev>
2019-11-14 11:40:51 +1100

    Rename file

--- src/interfaces/rpc/user.ts
+++ src/interfaces/rpc/user.ts
@@ -1,9 +1,11 @@
 import { EmptyObject } from '../misc'
 
-//
+// #region Register
 
 export interface RegisterEmailRequest {
   email: string
 }
 
 export type RegisterEmailResponse = EmptyObject
+
+// #endregion Register
