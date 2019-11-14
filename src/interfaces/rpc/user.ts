45e9b0c9b94a7123e64d3c2d336e3f1800b08023
Travis <travis@localhost.dev>
2019-11-14 11:48:25 +1100

    Implement RPC methods for User's registration section

--- src/interfaces/rpc/user.ts
+++ src/interfaces/rpc/user.ts
@@ -8,4 +8,34 @@ export interface RegisterEmailRequest {
 
 export type RegisterEmailResponse = EmptyObject
 
-// #endregion Register
+export interface RegisterEmailWithSecretRequest {
+  email: string
+  secret: string
+}
+
+export interface RegisterEmailWithSecretResponse {
+  dynamic_token: string
+}
+
+export interface UpdatePasswordRequest {
+  dynamic_token: string
+  password: string
+  token_type: string
+}
+
+export type UpdatePasswordResponse = EmptyObject
+
+export interface UpdatePasswordJwtRequest {
+  access_token: string
+  password: string
+}
+
+export type UpdatePasswordJwtResponse = EmptyObject
+
+export interface RequestPasswordResetRequest {
+  email: string
+}
+
+export type RequestPasswordResetResponse = EmptyObject
+
+// #endregion
--- src/rpc/user.ts
+++ src/rpc/user.ts
@@ -1,13 +1,47 @@
 import { invoke } from './base'
-import { RegisterEmailRequest, RegisterEmailResponse } from '../interfaces'
+import {
+  RegisterEmailRequest,
+  RegisterEmailResponse,
+  RegisterEmailWithSecretResponse,
+  RegisterEmailWithSecretRequest,
+  UpdatePasswordRequest,
+  UpdatePasswordResponse,
+  UpdatePasswordJwtRequest,
+  UpdatePasswordJwtResponse,
+  RequestPasswordResetRequest,
+  RequestPasswordResetResponse,
+} from '../interfaces'
 
 const defaultId: string = '0'
 const defaultMethodVersion: number = 1
 
 export class AsteroidUserRpc {
+  // #region Register
+
   static async registerEmail(rpcUrl: string, params: RegisterEmailRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<RegisterEmailResponse> {
     const method = 'User.RegisterEmail'
-    const res = await invoke(rpcUrl, method, params, id, methodVersion)
-    return res
+    return await invoke(rpcUrl, method, params, id, methodVersion)
   }
+
+  static async registerEmailWithSecret(rpcUrl: string, params: RegisterEmailWithSecretRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<RegisterEmailWithSecretResponse> {
+    const method = 'User.RegisterEmailWithSecret'
+    return await invoke(rpcUrl, method, params, id, methodVersion)
+  }
+
+  static async updatePassword(rpcUrl: string, params: UpdatePasswordRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<UpdatePasswordResponse> {
+    const method = 'User.UpdatePassword'
+    return await invoke(rpcUrl, method, params, id, methodVersion)
+  }
+
+  static async updatePasswordJwt(rpcUrl: string, params: UpdatePasswordJwtRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<UpdatePasswordJwtResponse> {
+    const method = 'User.UpdatePasswordJWT'
+    return await invoke(rpcUrl, method, params, id, methodVersion)
+  }
+
+  static async requestPasswordReset(rpcUrl: string, params: RequestPasswordResetRequest, id = defaultId, methodVersion = defaultMethodVersion): Promise<RequestPasswordResetResponse> {
+    const method = 'User.RequestPasswordReset'
+    return await invoke(rpcUrl, method, params, id, methodVersion)
+  }
+
+  // #endregion
 }
