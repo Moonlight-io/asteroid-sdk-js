7d14bb31611d03e1db78b2647e3cd18c00c6ae04
Travis <travis@localhost.dev>
2019-11-18 14:19:25 +1100

    Update RPC examples

--- examples/rpc.js
+++ examples/rpc.js
@@ -1,17 +1,50 @@
 const rpc = require('../dist').rpc
+const rest = require('../dist').rest
 
 // -- Implementation
+
+const aduVersion = async () => {
+  console.log('aduVersion triggered.')
+
+  const baseUrl = 'https://stage-user.asteroid.moonlight.io'
+  const res = await rest.user.getVersion(baseUrl)
+  console.log('res:', res)
+
+  console.log()
+}
+
+const aduRegisterEmail = async () => {
+  console.log('aduRegisterEmail triggered.')
+
+  const rpcUrl = 'https://stage-user.asteroid.moonlight.io/rpc'
+  params = {
+    email: 'travis+test_123@moonlight.io',
+  }
+  const res = await rpc.user.registerEmail(rpcUrl, params)
+  console.log('res:', res)
+
+  console.log()
+}
+
+const adwVersion = async () => {
+  console.log('adwVersion triggered.')
+
+  const baseUrl = 'https://stage-worker.asteroid.moonlight.io'
+  const res = await rest.user.getVersion(baseUrl)
+  console.log('res:', res)
+
+  console.log()
+}
 
 ;(async () => {
   console.log('== Example - RPC ==')
 
-  const url = 'https://stage-user.asteroid.moonlight.io/rpc'
-  const params = {
-    email: 'travis+test_123@moonlight.io',
-  }
+  // ADU
+  await aduVersion()
+  await aduRegisterEmail()
 
-  const res = await rpc.user.registerEmail(url, params)
-  console.log('res:', res)
+  // ADW
+  await adwVersion()
 
   console.log()
   console.log('== THE END ==')
