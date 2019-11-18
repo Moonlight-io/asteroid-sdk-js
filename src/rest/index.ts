6044b3635f52b319785c8037ed4d425ce0240d73
Travis <travis@localhost.dev>
2019-11-18 14:18:10 +1100

    Add REST API methods for ADW

--- src/rest/index.ts
+++ src/rest/index.ts
@@ -1,7 +1,8 @@
 import { AsteroidUserRest } from './user'
-
+import { AsteroidWorkerRest } from './worker'
 const rest = {
   user: AsteroidUserRest,
+  worker: AsteroidWorkerRest,
 }
 
 export { rest }
