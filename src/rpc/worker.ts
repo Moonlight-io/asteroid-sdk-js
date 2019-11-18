d27c3b9210fb1ea537e62b8db59354777dabf666
Travis <travis@localhost.dev>
2019-11-18 15:27:42 +1100

    Implement RPC methods for ADW

--- src/rpc/worker.ts
+++ src/rpc/worker.ts
@@ -1,6 +1,23 @@
 import { AxiosRequestConfig } from 'axios'
 import { invoke } from './base'
-import { ClaimTaskRequest, ClaimTaskResponse } from '../interfaces/api/worker'
+import {
+  ClaimTaskRequest,
+  ClaimTaskResponse,
+  CreateTaskRequest,
+  CreateTaskResponse,
+  GetActiveTaskIdsRequest,
+  GetActiveTaskIdsResponse,
+  GetTaskByIdRequest,
+  GetTaskByIdResponse,
+  GetUnclaimedTaskRequest,
+  GetUnclaimedTaskResponse,
+  ResolveTaskRequest,
+  ResolveTaskResponse,
+  UnclaimTaskRequest,
+  UnclaimTaskResponse,
+  RegisterWorkerRequest,
+  RegisterWorkerResponse,
+} from '../interfaces/api/worker'
 import { rpcDefaults } from '../constants'
 
 export class AsteroidWorkerRpc {
@@ -11,9 +28,44 @@ export class AsteroidWorkerRpc {
     return await invoke(rpcUrl, method, params, id, methodVersion, config)
   }
 
+  static async createTask(rpcUrl: string, params: CreateTaskRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<CreateTaskResponse> {
+    const method = 'Worker.CreateTask'
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
+  }
+
+  static async getActiveTaskIds(rpcUrl: string, params: GetActiveTaskIdsRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<GetActiveTaskIdsResponse> {
+    const method = 'Worker.GetActiveTaskIDs'
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
+  }
+
+  static async getTaskById(rpcUrl: string, params: GetTaskByIdRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<GetTaskByIdResponse> {
+    const method = 'Worker.GetTaskByID'
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
+  }
+
+  static async getUnclaimedTask(rpcUrl: string, params: GetUnclaimedTaskRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<GetUnclaimedTaskResponse> {
+    const method = 'Worker.GetUnclaimedTask'
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
+  }
+
+  static async resolveTask(rpcUrl: string, params: ResolveTaskRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<ResolveTaskResponse> {
+    const method = 'Worker.ResolveTask'
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
+  }
+
+  static async unclaimTask(rpcUrl: string, params: UnclaimTaskRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<UnclaimTaskResponse> {
+    const method = 'Worker.UnclaimTask'
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
+  }
+
   // #endregion
 
   // #region Workers
+
+  static async registerWorker(rpcUrl: string, params: RegisterWorkerRequest, id = rpcDefaults.id, methodVersion = rpcDefaults.methodVersion, config?: AxiosRequestConfig): Promise<RegisterWorkerResponse> {
+    const method = 'Worker.RegisterWorker'
+    return await invoke(rpcUrl, method, params, id, methodVersion, config)
+  }
 
   // #endregion
 }
