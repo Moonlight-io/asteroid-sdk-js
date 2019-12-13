810c7d58033270d33fcd1db56f3987be355027df
Travis <travis@localhost.dev>
2019-12-13 15:03:19 +1100

    Scribble implementation for neo name service

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export * from './constants'
 export * from './interfaces'
 export * from './rpc'
 export * from './rest'
-export * from './contracts'
+export * from './blockchain'
 
 // export * from './asteroid-user'
 export * from './asteroid'
