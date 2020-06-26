d1013abf888d5f5f2bce5846c7fb36be74cb2227
Travis <rockacola@gmail.com>
2020-06-26 20:43:38 +1000

    Update transpiled files

--- dist/validators/attribute-validator.d.ts
+++ dist/validators/attribute-validator.d.ts
@@ -3,7 +3,7 @@ export declare class AttributeValidator {
     static validate(attr: UserAttribute): void;
     static validateCoreRules(attr: UserAttribute, attributesCoreRules: AttributeCoreRules): void;
     static getAttributeValidationItem(attributeType: string): AttributeValidationItem | undefined;
-    static validProperty(propertyKey: string, propertyValue: any, rules: PropertyValidationRules): void;
+    static validProperty(attributeType: string, propertyKey: string, propertyValue: any, rules: PropertyValidationRules): void;
     private static createError;
     private static validateDateRangeOrder;
     private static getAttributesValidationRules;
